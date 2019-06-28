fe36536b9db139558e9707e50da47075e53cc3c7
kuhblume <kasumigaura0524@yahoo.co.jp>
2019-06-28 19:47:37 +0900

    データベース仮設

---
+++ database/migrations/2019_06_28_084137_create_buttons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateButtonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buttons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedInteger('device_id');
+            $table->text('ir_code');
+            $table->timestamps();
+
+//            $table->foreign('device_id')
+//                ->references('id')
+//                ->on('devices')
+//                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buttons');
+    }
+}
