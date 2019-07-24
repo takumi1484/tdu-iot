3e7b0099a0993fc5187b78486734fcc0a9a1c3f4
celorin <terminus1192@gmail.com>
2019-07-25 01:41:47 +0900

    いけえええええええ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,7 @@
                                 {{--                                <button type="submit" class="btn2">削除</button>--}}
                                 <form method="POST"  action="{{ action('IRController@updateIR', ['id' => $button->id])}}">
                                     @csrf
-                                    <button type="submit" class="btn1" class="trash_btn">&nbsp&nbsp{{$button->name}}</button>
+                                    <button type="submit" class="btn1">&nbsp&nbsp{{$button->name}}</button>
                                 </form>
                             </div>
                             <button type="submit" class="btn2" onclick="location.href='{{url('button/edit/'.$button->id)}}'">編集</button>
