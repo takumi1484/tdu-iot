4ba04273648933c7b121c6faaebbaa4b9e9c9994
celorin <terminus1192@gmail.com>
2019-08-01 03:31:32 +0900

    未ログインユーザーのページアクセスを排除

--- app/Http/Controllers/EditButtonController.php
+++ app/Http/Controllers/EditButtonController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Auth;
 
 class EditButtonController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index($id)
     {
         return view('editbtn')->with([
--- app/Http/Controllers/IRController.php
+++ app/Http/Controllers/IRController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\Auth;
 
 class IRController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function updateIR($id){
 
 
