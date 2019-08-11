834291791ccb28ba8c107ce437538d368cb9f0a3
fiji18110 <ikuy.s.fw11@gmail.com>
2019-08-11 23:57:01 +0900

    バグ修正

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::post('/button/{id}','IRController@updateIR');
 
 Route::get('/study/start','apiController@startStudy');
 
-Route::get('/send/{user_name}','apiController@getCode');
+Route::post('/send/{user_name}','apiController@getCode');
 
 Route::get('/unregister',function (){ return view('unregister');});
 
