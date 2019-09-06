a58b346d0d48d5ce3f6da6c97d08f312ba3bace7
fu2maim <miis.yt.vid@gmail.com>
2019-09-06 14:16:09 +0900

    装飾諸々

--- resources/views/searchResults.blade.php
+++ resources/views/searchResults.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+<link href="{{ asset('css/search_result.css') }}" rel="stylesheet">
 @section('content')
     <div class="search_device">
         {{--<form method="POST" action="{{ action('HomeController@addDevice')}}">
@@ -18,24 +19,26 @@
             <input type="submit" class="button" value="検索">
         </form>
         <input type="button" class="button" value="戻る" onclick="location.href='{{url('/')}}'"><br><br>--}}
-        <table border="1">
-            <tr align="center"><th>区分名</th><th>製品名</th><th>メーカー名</th><th>コピーする</th></tr>
-        <?php $i=0; ?>
-        @foreach($devices as $device)
-            <tr align="center">
-                <td>{{$device->name}}</td><td>{{$device->product}}</td><td>{{$device->manufacturer}}</td>
-                <td>
-            <form method="POST"  action="{{ action('HomeController@copyDevice')}}">
-                @csrf
-                <input type="hidden" name = "id"value="{{ $device->id }}">
-                <button type="submit">選択</button>
-            </form>
-                </td>
-            </tr>
-            <?php $i++; ?>
-        @endforeach
-        </table>
-        <p>検索結果は{{$i}}件です</p>
+        <div align="center">
+            <table class="result" border="1">
+                <tr align="center"><th>区分名</th><th>製品名</th><th>メーカー名</th><th>コピーする</th></tr>
+            <?php $i=0; ?>
+            @foreach($devices as $device)
+                <tr align="center">
+                    <td>{{$device->name}}</td><td>{{$device->product}}</td><td>{{$device->manufacturer}}</td>
+                    <td>
+                <form method="POST"  action="{{ action('HomeController@copyDevice')}}">
+                    @csrf
+                    <input type="hidden" name = "id"value="{{ $device->id }}">
+                    <button type="submit" class="bt">選択</button>
+                </form>
+                    </td>
+                </tr>
+                <?php $i++; ?>
+            @endforeach
+            </table>
+            <p>検索結果は{{$i}}件です</p>
+        </div>
 
     </div>
 @endsection
