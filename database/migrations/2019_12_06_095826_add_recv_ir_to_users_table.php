8ff30095d015c01fb2e0d4434f454fc7b84fdab6
celorin <terminus1192@gmail.com>
2019-12-13 16:33:33 +0900

    users tabele recv_ir set default string

--- database/migrations/2019_12_06_095826_add_recv_ir_to_users_table.php
+++ database/migrations/2019_12_06_095826_add_recv_ir_to_users_table.php
@@ -14,7 +14,7 @@ class AddRecvIrToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('recv_ir');
+            $table->string('recv_ir')->default("default");
         });
     }
 
